79b38cbe8f8e4b53a0d73d94fc62418284a828f1
Denis Kozlov <yadenisko1989@gmail.com>
2025-04-24 21:29:06 +0300

    add docker, add README

--- app/repository/db.py
+++ app/repository/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:030990@localhost/tasks")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:030990@db/tasks")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
